c9f69b4c955878edcdfce9cef5d7e70d4c2922fe
jwilson42 <wilson.jd022@gmail.com>
2021-02-02 15:29:57 -0800

    added the DELETE function of the CRUD app

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const fs = require('fs');
+const methodOverride = require("method-override");
 
 // Middle Ware - So, the code that we use inbetween the frontend and backend (routes); (Although it's written in the backend);
 app.use(expressLayouts);
 
 app.set('view engine', 'ejs');
 
-//body-parser middleware
+//body-parser middleware for reading POST
 app.use(express.urlencoded({extended: false}));
+
+// Method Overide for PUT and DELETE
+app.use(methodOverride('_method'));
 
 // ROUTES
 
@@ -22,13 +26,26 @@ app.get('/dinosaurs', (req, res) => {
     let dinos = fs.readFileSync(`${__dirname}/dinosaurs.json`);
     dinos = JSON.parse(dinos);
     
-    if (Object.keys(req.query).length != 0) {
+    // If there was a query assume it's a name filter
+    if (req.query.nameFilter) {
         let dinoID = req.query.nameFilter;
-        let dino = dinos.filter((dino) => dino.name.toLowerCase() === dinoID.toLowerCase())[0];
+        let index = null;
 
-        res.render(`dinosaurs/show.ejs`, { dino });
+        for (let i = 0; i < dinos.length; i++) {
+            if (dinos[i].name.toLowerCase() === dinoID.toLowerCase()) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index) {
+            res.redirect(`/dinosaurs/${index}`);
+        } else {
+            res.status(400);
+            res.redirect('/dinosaurs');
+        }
+
     }
-
     // Render the basic dino page with dino array.
     res.render(`dinosaurs/index.ejs`, { dinos });
 });
@@ -50,6 +67,8 @@ app.get('/dinosaurs/:id', (req, res) => {
     res.render(`dinosaurs/show.ejs`, { dino });
 });
 
+// POST
+
 app.post("/dinosaurs", (req, res) => {
     // Update the Database
     let dinos = fs.readFileSync(`${__dirname}/dinosaurs.json`);
@@ -57,12 +76,26 @@ app.post("/dinosaurs", (req, res) => {
     dinos.push(req.body);
 
     // Rewrite the json file.
-    dinos = JSON.stringify(dinos);
-    fs.writeFileSync(`${__dirname}/dinosaurs.json`, dinos);
+    fs.writeFileSync(`${__dirname}/dinosaurs.json`, JSON.stringify(dinos));
 
     // With a redirect, we go to a new route by GET method.
     res.redirect('/dinosaurs');
 }); 
+
+// DELETE
+app.delete('/dinosaurs/:id', (req, res) => {
+    const dinosArray = JSON.parse(fs.readFileSync(`${__dirname}/dinosaurs.json`));
+    const dinoID = parseInt(req.params.id);
+
+    // Remove Dino
+    dinosArray.splice(dinoID, 1);
+
+    // Rewrite to files
+    fs.writeFileSync(`${__dirname}/dinosaurs.json`, JSON.stringify(dinosArray));
+
+    // With a redirect, we go to a new route by GET method.
+    res.redirect('/dinosaurs');
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
