8f477b34ad261cabac2c168b248bd9e040531dd0
jwilson42 <wilson.jd022@gmail.com>
2021-02-02 16:39:48 -0800

    Added the update feature to the CRUD app and correctly mapped routes to make contious loop through all routes

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ app.use(methodOverride('_method'));
 // ROUTES
 
 app.get('/', (req, res) => {
-    res.send("HI");
+    res.redirect('/dinosaurs');
 });
 
+// Main DINO LIST PAGE
 app.get('/dinosaurs', (req, res) => {
     // Access the database
     let dinos = fs.readFileSync(`${__dirname}/dinosaurs.json`);
@@ -31,30 +32,43 @@ app.get('/dinosaurs', (req, res) => {
         let dinoID = req.query.nameFilter;
         let index = null;
 
+        // Returns first occurance of a dino with the filered name
         for (let i = 0; i < dinos.length; i++) {
             if (dinos[i].name.toLowerCase() === dinoID.toLowerCase()) {
                 index = i;
                 break;
             }
         }
-
         if (index) {
             res.redirect(`/dinosaurs/${index}`);
         } else {
             res.status(400);
             res.redirect('/dinosaurs');
         }
-
     }
     // Render the basic dino page with dino array.
     res.render(`dinosaurs/index.ejs`, { dinos });
 });
 
+// TO NEW PAGE
 app.get('/dinosaurs/new', (req, res) => {
     // Render the form for making new dinos.
     res.render("dinosaurs/new.ejs");
 });
 
+// TO EDIT PAGE
+app.get("/dinosaurs/edit/:id", (req, res) => {
+    const dinosArray = JSON.parse(fs.readFileSync(`${__dirname}/dinosaurs.json`));
+    const dinoID = parseInt(req.params.id);
+    let dino = {
+        id: dinoID,
+        name: dinosArray[dinoID].name,
+        type: dinosArray[dinoID].type
+    }
+    res.render("dinosaurs/edit.ejs", { dino })
+});
+
+// TO SPECIFIC DINO PAGE
 app.get('/dinosaurs/:id', (req, res) => {
     // Read from Database
     let dinos = fs.readFileSync(`${__dirname}/dinosaurs.json`);
@@ -67,8 +81,8 @@ app.get('/dinosaurs/:id', (req, res) => {
     res.render(`dinosaurs/show.ejs`, { dino });
 });
 
-// POST
 
+// POST
 app.post("/dinosaurs", (req, res) => {
     // Update the Database
     let dinos = fs.readFileSync(`${__dirname}/dinosaurs.json`);
@@ -94,6 +108,24 @@ app.delete('/dinosaurs/:id', (req, res) => {
     fs.writeFileSync(`${__dirname}/dinosaurs.json`, JSON.stringify(dinosArray));
 
     // With a redirect, we go to a new route by GET method.
+    res.redirect('/dinosaurs');
+});
+
+// PUT
+app.put('/dinosaurs/:id', (req, res) => {
+    // Access the database
+    const dinosArray = JSON.parse(fs.readFileSync(`${__dirname}/dinosaurs.json`));
+    const dinoID = parseInt(req.params.id);
+
+    // Take data from query
+    const dino = dinosArray[dinoID];
+    dino.name = req.body.name;
+    dino.type = req.body.type;
+
+    // UPDATE the database
+    dinosArray[dinoID] = dino;
+    fs.writeFileSync(`${__dirname}/dinosaurs.json`, JSON.stringify(dinosArray));
+
     res.redirect('/dinosaurs');
 });
 
